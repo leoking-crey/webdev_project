9485d4c542b10df1e1bb7a9a5dad91e1a712f820
abhinav <abhinav5898@gmail.com>
2018-03-19 20:23:24 +0530

    a
    Signed-off-by: abhinav <abhinav5898@gmail.com>

--- app/routes.js
+++ app/routes.js
@@ -1,26 +1,28 @@
 const express = require('express')
 const app = express();
-const passport = require('passport')
+const path = require('path');
+const passport = require('passport-local')
 module.exports = function(app,passport)
 {    
-app.get('/', function(req, res) {
+app.get('/',() => console.log("a"), function(req, res) {
         res.render('index'); // load the index.ejs file
     });
-app.get('/signup', function(req,res) {
-    res.render('index');
+app.get('/signup',() => console.log("b"), function(req,res) {
+    res.sendHTML(path.join(__dirname,'../intro'));_
    // res.successRedirect('../chat');
-})
-app.get('/profile', function(req,res) {
-    res.render('../chat/index.html');
-})
+});
 
-app.post('/signup', passport.authenticate('local-signup', {
-        successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/signup' // redirect back to the signup page if there is an error
+app.post('/signup', () => console.log("Hello"), passport.authenticate('local-signup', {
+        successRedirect : '../chat/index.html', // redirect to the secure profile section
+        failureRedirect : '/login' // redirect back to the signup page if there is an error
     }));    
-}
 
-app.post('/login', passport.authenticate('local-login',{
+app.post('/login',() => console.log("c"), passport.authenticate('login',{
     successRedirect : '/profile',
     failureRedirect : '/signup'
 }));
+
+app.get('/profile',() => console.log("d"), function(req,res) {
+    res.sendHTML(path.join(__dirname,'../chat'));
+});
+}
